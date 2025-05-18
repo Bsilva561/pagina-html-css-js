e10f2afd438a7391c993ee6d4ed29785213da959
Bsilva561 <silva561@hotmail.com>
2025-05-18 18:03:12 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+function mostrarMensagem() {
+  const div = document.getElementById("mensagem");
+  div.textContent = "Seja bem-vindo! Nunca pare de aprender.";
+  div.style.marginTop = "10px";
+  div.style.fontWeight = "bold";
+}
